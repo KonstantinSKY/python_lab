84d4b0add2957961f929ab9a8e1e0bd0aafafd42
Konstantin Smolnikov <sky012877@gmail.com>
2020-08-11 14:36:21 -0700

    Created JSON decorator

---
+++ decor_json.py
@@ -0,0 +1,11 @@
+import functools
+import json
+
+
+def to_json(func):            # Decorator to json
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        return json.dumps(func(*args, **kwargs))
+
+    return inner
+
