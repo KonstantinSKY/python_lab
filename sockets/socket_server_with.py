6e72fa5928e5d8295d98f28e426a3fbf0e07dcc1
Konstantin Smolnikov <sky012877@gmail.com>
2020-08-14 23:53:23 -0700

    sockets servers and clients

---
+++ sockets/socket_server_with.py
@@ -0,0 +1,22 @@
+# Creating socket server with context manager
+
+import socket
+
+with socket.socket() as sock:
+    sock.bind(("", 10001))
+    sock.listen()
+
+    while True:
+        conn, addr = sock.accept()
+        with conn:
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                print(data.decode("utf8"))
+
+# клиент
+import socket
+
+with socket.create_connection(("127.0.0.1", 10001)) as sock:
+    sock.sendall("ping".encode("utf8"))
