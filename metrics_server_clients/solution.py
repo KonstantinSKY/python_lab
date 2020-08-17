ee30f768ed49edba211b0b6e29391283fd775177
Konstantin Smolnikov <sky012877@gmail.com>
2020-08-16 22:50:47 -0700

    finished Client v1

--- metrics_server_clients/solution.py
+++ metrics_server_clients/solution.py
@@ -20,40 +20,49 @@ class Client:
 
     def get(self, metric_name):
         print(f'this is GET, metric name {metric_name}')
-        self.sock.sendall(f"get \n".encode("utf8"))
-        data = self.sock.recv(1024).decode()
+        try:
+            self.sock.sendall(f"get {metric_name}\n".encode("utf8"))
+            data = self.sock.recv(1024).decode()
+        except ClientError:
+            raise ClientError()
         data_list = data.rsplit("\n")
-        if data_list == 'error':
+
+        if data_list[0] == 'error':
             raise ClientError()
         print(f"Data got OK {data}")
-        print(f"Data_list OK {data_list}")
+        print(f"Data_list OK {data_list[0]}")
         del data_list[0]
         print(f"Data_list {data_list}")
         data_one = [data.split(" ") for data in data_list if data != '']
         res = {}
-        for data in data_one:
-            if data[0] not in res:
-                res[data[0]] = []
-            res[data[0]].append((int(data[2]), float(data[1])))
+        try:
+            for data in data_one:
+                if data[0] not in res:
+                    res[data[0]] = []
+                res[data[0]].append((int(data[2]), float(data[1])))
+        except Exception:
+            raise ClientError()
+        [value.sort() for key, value in res.items()]
         print(res)
-        #res2 = dict([value.sort(key=value[0]) for key, value in res.items()])
-        # for key, value in res.items():
-        #     print (value)
-        print(res2)
+        return res
 
     def put(self, metric_name, value, timestamp=None):
         print(f'This is PUT metric = {metric_name}, value = {value}, ts = {timestamp}')
         if timestamp is None:
-            timestamp = int(time.time())
+            timestamp = time.time()
+        else:
+            try:
+                timestamp = int(timestamp)
+            except Exception:
+                pass
         try:
-            timestamp = int(timestamp)
-        except Exception:
-            print("timestamp incorrect value")
-            timestamp = int(time.time())
+            self.sock.sendall(f"put {metric_name} {value} {timestamp}\n".encode("utf8"))
+            data = self.sock.recv(1024).decode()
+        except ClientError():
+            raise ClientError
 
-        self.sock.sendall(f"put {metric_name} {value} {timestamp}\n".encode("utf8"))
-        data = self.sock.recv(1024).decode()
         data_list = data.rsplit("\n")
-        if data_list == 'error':
+        if data_list[0] == 'error':
             raise ClientError()
+
         print(f"Put response: {data}")
