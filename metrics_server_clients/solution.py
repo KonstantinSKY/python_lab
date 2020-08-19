bcdf4e7e89d57acbbf15b53897516a763d795179
Konstantin Smolnikov <sky012877@gmail.com>
2020-08-18 18:53:31 -0700

    last step of metrics server

--- metrics_server_clients/solution.py
+++ metrics_server_clients/solution.py
@@ -1,97 +1,90 @@
 import asyncio
 
 data_storage = {}
+error_string = "error\nwrong command\n\n"
+
+
+def run_server(host, port):
+    loop = asyncio.get_event_loop()
+
+    coro = loop.create_server(
+        ClientServerProtocol,
+        host, port
+    )
+
+    server = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+
+    server.close()
+    loop.run_until_complete(server.wait_closed())
+    loop.close()
 
 
 def _get(data):
     data = data[4:-1]
     resp_str = "ok\n"
 
+    if " " in data:
+        return error_string
+
     if data == "*":
         for key, metrics in data_storage.items():
-            for metric in metrics:
-                resp_str += f"{key} {metric[1]} {metric[0]}\n"
+            for ts, metric in metrics.items():
+                resp_str += f"{key} {metric} {ts}\n"
         resp_str += "\n"
-        print(resp_str)
         return resp_str
 
     if data not in data_storage:
         resp_str += "\n"
-        print(resp_str)
         return resp_str
 
-    for metrics in data_storage[data]:
-        resp_str += f"{data} {metrics[1]} {metrics[0]}\n"
+    for ts, metric in data_storage[data].items():
+        resp_str += f"{data} {metric} {ts}\n"
+
     resp_str += "\n"
-    print(resp_str)
+
     return resp_str
 
 
 def _put(data):
     data_list = data[4:-1].split(" ")
-    print(data_list)
     if len(data_list) != 3:
-        print("Error len")
-        return "error\nwrong command\n\n"
+        return error_string
     try:
-        metric = [
-                int(data_list[2]),
-                float(data_list[1])
-            ]
+        metric = {int(data_list[2]): float(data_list[1])}
+
     except Exception:
-        print("Error metric")
-        return "error\nwrong command\n\n"
+        return error_string
 
     if data_list[0] in data_storage:
-        data_storage[data_list[0]].append(metric)
+        data_storage[data_list[0]].update(metric)
     else:
-        data_storage.update({data_list[0]: [metric]})
-    print(data_storage)
+        data_storage.update({data_list[0]: metric})
     return "ok\n\n"
 
 
 def process_data(data):
     data = str(data)
     if data.startswith("put ") and data.endswith("\n"):
-        print("put")
         return _put(data)
     elif data.startswith("get ") and data.endswith("\n"):
-        print("get")
         return _get(data)
     else:
-        print("Wrong command err process data")
         return "error\nwrong command\n\n"
 
 
 class ClientServerProtocol(asyncio.Protocol):
     def __init__(self):
-        self.transport = None
+        pass
 
     def connection_made(self, transport):
         self.transport = transport
 
     def data_received(self, data):
         resp = process_data(data.decode())
-        print(data)
-        print(resp)
+
         self.transport.write(resp.encode())
-
-
-loop = asyncio.get_event_loop()
-print(loop)
-coro = loop.create_server(
-    ClientServerProtocol,
-    '127.0.0.1', 8888
-)
-
-server = loop.run_until_complete(coro)
-print(server)
-
-try:
-    loop.run_forever()
-except KeyboardInterrupt:
-    pass
-
-server.close()
-loop.run_until_complete(server.wait_closed())
-loop.close()
--- metrics_server_clients/start.py
+++ metrics_server_clients/start.py
@@ -1,4 +1,4 @@
-from metrics_client import Client
+from client import Client
 
 client = Client("127.0.0.1", 8888, timeout=15)
 
@@ -7,6 +7,7 @@ client.put("palm.cpu1", 0.4, timestamp=1150964247)
 client.put("palm.cpu3", 3.5, timestamp=1150264247)
 client.put("palm.cpu", 0.34, timestamp=1150864248)
 client.put("palm.cpu", 0.54, timestamp=1140864247)
+client.put("palm.cpu", 0.12154, timestamp=1140864247)
 print(client.get("*"))
 print(client.get("palm.cpu"))
 print(client.get("palmas.cpu"))
