5bd484c04eb48c5797a5ff12f7b2f57bb5da5ede
Konstantin Smolnikov <sky012877@gmail.com>
2020-08-20 16:46:33 -0700

    abstract class step 2 and test for it

--- abstract_class/abc_classes.py
+++ abstract_class/abc_classes.py
@@ -1,18 +1,24 @@
 import math
+from abc import ABC, abstractmethod
 
 
-class Base:
-
-    pass
-
-
-class A:
+class Base(ABC):
     def __init__(self, data, result):
         self.data = data
         self.result = result
+    pass
 
+    @abstractmethod
     def get_answer(self):
         return [int(x >= 0.5) for x in self.data]
+
+
+class A(Base):
+    def __init__(self, data, result):
+        super().__init__(data, result)
+
+    def get_answer(self):
+        return super().get_answer()
 
     def get_score(self):
         ans = self.get_answer()
@@ -23,13 +29,9 @@ class A:
             [(x - y) * (x - y) for (x, y) in zip(self.data, self.result)])
 
 
-class B:
+class B(Base):
     def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
+        super().__init__(data, result)
 
     def get_loss(self):
         return -sum([
@@ -53,18 +55,13 @@ class B:
         return 2 * pre * rec / (pre + rec)
 
 
-class C:
+class C(Base):
     def __init__(self, data, result):
-        self.data = data
-        self.result = result
-
-    def get_answer(self):
-        return [int(x >= 0.5) for x in self.data]
+        super().__init__(data, result)
 
     def get_score(self):
         ans = self.get_answer()
-        return sum([int(x == y) for (x, y) in zip(ans, self.result)]) \
-            / len(ans)
+        return sum([int(x == y) for (x, y) in zip(ans, self.result)]) / len(ans)
 
     def get_loss(self):
         return sum([abs(x - y) for (x, y) in zip(self.data, self.result)])
---
+++ abstract_class/test_abs_classes.py
@@ -0,0 +1,24 @@
+import unittest
+from abc_classes import *
+
+
+class TestABCClasses(unittest.TestCase):
+    def setUp(self):
+        self.case_data = [231, 34.343, -0.23, -131, 0.3, 434.2, 232]
+        self.case_res = [23321, 234.243, 20.3]
+        self.a = A(self.case_data, self.case_res)
+
+    def test_init(self):
+        self.assertEqual(self.a.data, self.case_data)
+        self.assertEqual(self.a.result, self.case_res)
+
+    def test_get_answer(self):
+        case = [1, 1, 0, 0, 0, 1, 1]
+        self.assertEqual(self.a.get_answer(), case)
+
+    def tearDown(self):
+        del self.a
+
+
+ if __name__ == "__main__":
+    unittest.main()
